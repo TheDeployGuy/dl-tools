d388f3d22ddd728586a647ee71e40253cb94bc40
thedeployguy <jason.lloyddev@gmail.com>
2018-04-20 21:59:59 +0100

    Add SubsForm component for our simple form with a few input forms that do nothing at the moment

---
+++ subs-web-application/front-end/src/SubsForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class SubsForm extends Component {
+
+
+    state = {
+
+    }
+
+    render() {
+        return (
+        <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Player name</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter player name" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Player's Team</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter players club e.g. D2" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Sub's Paid</label>
+              <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Subs Paid e.g. €100" />
+            </div>
+            <button type="submit" class="btn btn-primary text-right">Add Subs</button>
+        </form>
+        )
+    }
+}
+
+export default SubsForm;
