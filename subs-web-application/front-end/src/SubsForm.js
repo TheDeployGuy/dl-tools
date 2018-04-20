6fbdee31b20c72dd5d424742ebc58a960483769a
thedeployguy <jason.lloyddev@gmail.com>
2018-04-20 22:40:02 +0100

    Give body some padding and some form elements some styling

--- subs-web-application/front-end/src/SubsForm.js
+++ subs-web-application/front-end/src/SubsForm.js
@@ -12,15 +12,20 @@ class SubsForm extends Component {
         <form>
             <div class="form-group">
               <label for="exampleInputEmail1">Player name</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter player name" />
+              <input type="text" class="form-control form-border" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter player name" />
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Player's Team</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter players club e.g. D2" />
+              <input type="text" class="form-control form-border" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter players club e.g. D2" />
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Sub's Paid</label>
+              <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">€</div>
+              </div>
               <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Subs Paid e.g. €100" />
+              </div>
             </div>
             <button type="submit" class="btn btn-primary text-right">Add Subs</button>
         </form>
