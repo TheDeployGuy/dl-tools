c3c53577972bb7e8514b7dfca03072749ea7a987
thedeployguy <jason.lloyddev@gmail.com>
2018-04-21 12:19:21 +0100

    Implement simple autocomplete for teams

--- subs-web-application/front-end/src/SubsForm.js
+++ subs-web-application/front-end/src/SubsForm.js
@@ -7,6 +7,8 @@ class SubsForm extends Component {
         name: '',
         team: '',
         paid: 0,
+        teams: ['National League', 'D1', 'D2', 'D3', 'D4'],
+        autocomplete: []
     }
 
     onSubmit = async (e) => {
@@ -25,6 +27,17 @@ class SubsForm extends Component {
     handleInputChange = (e) => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
+        if (name === 'team') {
+            this.filterAutocomplete();
+        }
+    }
+
+    filterAutocomplete = () => {
+        this.setState({ autocomplete: this.state.teams.filter(team => team.toLowerCase().startsWith(this.state.team.toLowerCase())) })
+    }
+
+    handleSelectedAutocomplete = (name) => {
+        this.setState({ team: name, autocomplete: [] });
     }
 
     render() {
@@ -37,6 +50,9 @@ class SubsForm extends Component {
             <div className="form-group">
               <label htmlFor="inputTeam">Player's Team</label>
               <input type="text" className="form-control form-border" id="inputTeam" name="team" placeholder="Enter players club e.g. D2" value={this.state.team} onChange={this.handleInputChange} />
+              <ul className="list-group">
+                { this.state.autocomplete.map(team => <li className="list-group-item" key={team} onClick={() => this.handleSelectedAutocomplete(team)}>{team}</li> ) }
+              </ul>
             </div>
             <div className="form-group">
               <label htmlFor="inputPaid">Sub's Paid</label>
@@ -47,7 +63,7 @@ class SubsForm extends Component {
               <input type="number" className="form-control" id="inputPaid" name="paid" placeholder="100" value={this.state.paid} onChange={this.handleInputChange} />
               </div>
             </div>
-            <button type="submit" className="btn btn-primary text-right">Add Subs</button>
+            <button type="submit" className="btn btn-primary btn-block ">Add Subs</button>
         </form>
         )
     }
