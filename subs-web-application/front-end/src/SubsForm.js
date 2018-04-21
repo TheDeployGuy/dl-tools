881fb33d22aba9ea36fecc69275759a2bcc6cc0c
thedeployguy <jason.lloyddev@gmail.com>
2018-04-21 11:20:24 +0100

    Add functions to handle form input changes and to placeholder code for creating new entry

--- subs-web-application/front-end/src/SubsForm.js
+++ subs-web-application/front-end/src/SubsForm.js
@@ -4,30 +4,50 @@ class SubsForm extends Component {
 
 
     state = {
+        name: '',
+        team: '',
+        paid: 0,
+    }
 
+    onSubmit = async (e) => {
+        e.preventDefault();
+    
+        const { name, team, paid } = this.state;
+        const newEntry = {
+          name,
+          team,
+          paid,
+        };
+
+        console.log(newEntry)
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
     }
 
     render() {
         return (
-        <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Player name</label>
-              <input type="text" class="form-control form-border" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter player name" />
+        <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="inputName">Player name</label>
+              <input type="text" className="form-control form-border" id="inputName" name="name" placeholder="Enter player name" value={this.state.name} onChange={this.handleInputChange} />
             </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Player's Team</label>
-              <input type="text" class="form-control form-border" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter players club e.g. D2" />
+            <div className="form-group">
+              <label htmlFor="inputTeam">Player's Team</label>
+              <input type="text" className="form-control form-border" id="inputTeam" name="team" placeholder="Enter players club e.g. D2" value={this.state.team} onChange={this.handleInputChange} />
             </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Sub's Paid</label>
-              <div class="input-group">
-              <div class="input-group-prepend">
-                <div class="input-group-text">€</div>
+            <div className="form-group">
+              <label htmlFor="inputPaid">Sub's Paid</label>
+              <div className="input-group">
+              <div className="input-group-prepend">
+                <div className="input-group-text">€</div>
               </div>
-              <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Subs Paid e.g. €100" />
+              <input type="number" className="form-control" id="inputPaid" name="paid" placeholder="100" value={this.state.paid} onChange={this.handleInputChange} />
               </div>
             </div>
-            <button type="submit" class="btn btn-primary text-right">Add Subs</button>
+            <button type="submit" className="btn btn-primary text-right">Add Subs</button>
         </form>
         )
     }
