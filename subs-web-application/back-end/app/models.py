c3c90660dc80708c10f6a859b796807192810966
thedeployguy <jason.lloyddev@gmail.com>
2018-04-23 21:27:44 +0100

    Add Entry table and migration files for it. Also setup our flask shell context for easier playing around in the future

--- subs-web-application/back-end/app/models.py
+++ subs-web-application/back-end/app/models.py
@@ -1,9 +1,29 @@
+from datetime import datetime
 from app import db
 
+# All models inherit from the Base Flask-SqlAlchemy Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # This is a high-level view of the relationship between users and entries. For a one-to-many relationship, a db.relationship field is normally defined on the 'one' side and is used as a convenient way to get access to the 'many' side. 
+    # Argument 1 = Which Model class represents the many side
+    # Argument 2 = Defines the name of a field that will be added to the objects of the 'many' class that points back at the 'one' object. 
+    # Argument 3 = Defines how the database query for the relationship will be issues
+    entries = db.relationship('Entry', backref='author', lazy='dynamic')
 
+    # This method tells Python how to print objects of this class.
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(200))
+    # Field will get indexed, useful for ordering and its utc time as default
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # This References an id value from the users table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __repr__(self):
+        return '<Entry {}>'.format(self.details)
--- subs-web-application/back-end/server.py
+++ subs-web-application/back-end/server.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Entry
+
+# Instead of using just the python interperator we can use the flask shell which will automatically import and configure the context for our application for easier debugging and playing around.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Entry': Entry}
