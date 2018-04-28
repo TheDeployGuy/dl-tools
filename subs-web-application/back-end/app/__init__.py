2409233ec40efade29589a387413884cf33ee80f
thedeployguy <jason.lloyddev@gmail.com>
2018-04-28 14:42:46 +0100

    Add tests for password hashing and for setup and teardown for usermodel

--- subs-web-application/back-end/app/__init__.py
+++ subs-web-application/back-end/app/__init__.py
@@ -17,9 +17,9 @@ login = LoginManager(app)
 if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/subs_api.log', maxBytes=10240, backupCount=10
+    file_handler = RotatingFileHandler('logs/subs_api.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')))
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
--- subs-web-application/back-end/tests.py
+++ subs-web-application/back-end/tests.py
@@ -4,4 +4,21 @@ from app import app, db
 from app.models import User, Entry
 
 class UserModelCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        # This creates an in-memory db for the tests to use.
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_password_hashing(self):
+        u = User(username='John')
+        u.set_password('super-secret-password')
+        self.assertFalse(u.check_password('passsword-secret'))
+        self.assertTrue(u.check_password('super-secret-password'))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
